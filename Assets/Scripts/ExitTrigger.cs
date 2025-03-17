238c681962a0b655c4d389e9ed20d6c116701f50
ABBush2001 <alex.bush@my.utsa.edu>
2025-03-17 16:34:56 -0500

    Added explosion outro

--- Assets/Scripts/ExitTrigger.cs
+++ Assets/Scripts/ExitTrigger.cs
@@ -18,6 +18,8 @@ public class ExitTrigger : MonoBehaviour
     public AudioSource ExplosionSound3;
     public AudioSource ExplosionSound4;
 
+    public float shakeAmount;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
@@ -33,18 +35,43 @@ public class ExitTrigger : MonoBehaviour
         yield return new WaitForSeconds(1);
         outroCamera.GetComponent<CameraFadeOut>().fadeOut = true;
         ExplosionSound1.Play();
+        StartCoroutine(cameraShake(1f, shakeAmount));
         Explosion1.GetComponent<ParticleSystem>().Play();
         yield return new WaitForSeconds(0.5f);
         ExplosionSound2.Play();
+        StartCoroutine(cameraShake(1f, shakeAmount));
         Explosion2.GetComponent<ParticleSystem>().Play();
         yield return new WaitForSeconds(0.2f);
         ExplosionSound3.Play();
+        StartCoroutine(cameraShake(1f, shakeAmount));
         Explosion3.GetComponent<ParticleSystem>().Play();
         yield return new WaitForSeconds(0.3f);
         ExplosionSound4.Play();
+        StartCoroutine(cameraShake(5f, shakeAmount));
         Explosion4.GetComponent<ParticleSystem>().Play();
 
         yield return new WaitForSeconds(6);
         SceneManager.LoadScene("2.Oceanfloor");
+    }
+
+    IEnumerator cameraShake(float duration, float magnitude)
+    {
+        Vector3 originalPos = outroCamera.transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while(elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            outroCamera.transform.localPosition += new Vector3(x, y, 0);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        outroCamera.transform.localPosition = originalPos;
     }
 }
