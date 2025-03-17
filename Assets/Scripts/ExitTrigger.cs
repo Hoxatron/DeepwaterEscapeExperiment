4dbae284323f46fcfb839994b48de442ad9c5886
ABBush2001 <alex.bush@my.utsa.edu>
2025-03-17 14:15:27 -0500

    Explosion FX in submarine

--- Assets/Scripts/ExitTrigger.cs
+++ Assets/Scripts/ExitTrigger.cs
@@ -6,6 +6,17 @@ using UnityEngine.SceneManagement;
 public class ExitTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject outroCamera;
+
+    public GameObject Explosion1;
+    public GameObject Explosion2;
+    public GameObject Explosion3;
+    public GameObject Explosion4;
+
+    public AudioSource ExplosionSound1;
+    public AudioSource ExplosionSound2;
+    public AudioSource ExplosionSound3;
+    public AudioSource ExplosionSound4;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,7 +28,22 @@ public class ExitTrigger : MonoBehaviour
 
     IEnumerator fadeToNextScene()
     {
-        mainCamera.GetComponent<CameraFadeOut>().fadeOut = true;
+        mainCamera.SetActive(false);
+        outroCamera.SetActive(true);
+        yield return new WaitForSeconds(1);
+        outroCamera.GetComponent<CameraFadeOut>().fadeOut = true;
+        ExplosionSound1.Play();
+        Explosion1.GetComponent<ParticleSystem>().Play();
+        yield return new WaitForSeconds(0.5f);
+        ExplosionSound2.Play();
+        Explosion2.GetComponent<ParticleSystem>().Play();
+        yield return new WaitForSeconds(0.2f);
+        ExplosionSound3.Play();
+        Explosion3.GetComponent<ParticleSystem>().Play();
+        yield return new WaitForSeconds(0.3f);
+        ExplosionSound4.Play();
+        Explosion4.GetComponent<ParticleSystem>().Play();
+
         yield return new WaitForSeconds(6);
         SceneManager.LoadScene("2.Oceanfloor");
     }
