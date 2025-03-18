42846ce7f62756f235e934537ec8f5afcccd0455
Hoxatron <derpman8000@gmail.com>
2025-03-17 21:23:59 -0500

    Update PlayerCollisionChecker.cs

--- Assets/Scripts/Level3Scripts/RailShooter/Player/PlayerCollisionChecker.cs
+++ Assets/Scripts/Level3Scripts/RailShooter/Player/PlayerCollisionChecker.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * This script handles checking if the OnRails player collides with an obstacle.
+ */
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,5 +17,13 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+
+        }
     }
 }
